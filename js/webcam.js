427f2250c7e16046d605ba9cb7377a08e64286a2
Abarcenas <abarcenas1111@gmail.com>
2020-11-08 22:39:39 -0800

    Image capturing on the webcam

--- js/webcam.js
+++ js/webcam.js
@@ -3,7 +3,11 @@ const liveView = document.getElementById('liveView');
 const viewing = document.getElementById('viewing');
 const enableWebcamButton = document.getElementById('webcamButton');
 
+const capture = document.getElementById("capture");
+const capButton = document.getElementById('capButton');
+const snapshot = document.getElementById('snapshot');
 
+var camerastream = null;
 
 function getUserMediaSupported() {
     return !!(navigator.mediaDevices &&
@@ -12,6 +16,7 @@ function getUserMediaSupported() {
 
 if (getUserMediaSupported()) {
     enableWebcamButton.addEventListener('click', enableCam);
+    capButton.addEventListener('click', captureSnapshot);
 } else {
     console.warn('getUserMedia() is not supported by your browser');
 }
@@ -26,8 +31,19 @@ function enableCam(event) {
 
     navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
         video.srcObject = stream;
+        camerastream = stream;
     });
 
 }
 
+function captureSnapshot() {
+    var ctx = capture.getContext('2d');
+    var img = new Image();
+
+    ctx.drawImage(video,0,0,capture.width, capture.height);
+    img.src = capture.toDataURL("image/png");
+    img.width = 240;
+    snapshot.innerHTML = '';
+    snapshot.appendChild(img);
+}
 
